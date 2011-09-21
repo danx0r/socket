8adbcd25dc6f4d4d6a0e493bae23f17e126b9317
dbm <dbm@kata-wallenberg.(none)>
2011-09-21 16:43:33 -0700

    beginnings of an ad-hack protocol

--- client.py
+++ client.py
@@ -1,13 +1,30 @@
-import socket
+import time, socket
 
 HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send('Hello, world')
-data = s.recv(1024)
-print 'Received', repr(data)
-s.send('GOODBYE')
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+PORT = 50007             # The same port as used by the server
+g_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+g_sock.connect((HOST, PORT))
+
+def sendMsg(s):
+    s = chr(len(s)) + s
+    print "sending-->" + repr(s) + "<--"
+    g_sock.send(s)
+
+def getMsg():
+    cnt = 0
+    while cnt == 0:
+        data = g_sock.recv(1)
+        if len(data):
+            cnt = ord(data[0])
+            data = g_sock.recv(cnt)
+    return data
+        
+
+sendMsg('abc')
+sendMsg('ack')
+x = getMsg()
+print "received:", x
+sendMsg('Holy Cow Batman!')
+sendMsg('')
+time.sleep(1)
+g_sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #listen on socket, echo double
-import socket
+import sys, time, socket
 
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 50007              # Arbitrary non-privileged port
@@ -10,10 +10,18 @@ print "test python server, waiting for connection"
 conn, addr = s.accept()
 print 'Connected by', addr
 while 1:
-    data = conn.recv(1024)
-    if not data: break
-    print "data received:", data
-    print "sending back:", data + data
-    conn.send(data + data)
+    data = conn.recv(1)
+    if len(data):
+        cnt = ord(data[0])
+        if cnt==0:
+            break
+        data = conn.recv(cnt)
+        print "received msg-->" + repr(data) + "<--"
+        if data == "ack":
+            conn.send(chr(3) + "ACK")
+    else:
+        print ".",
+        sys.stdout.flush()
+##    time.sleep(.1)
 print "closing connection"
 conn.close()
